d4da7a343704066eb3402710d4387e1e36f8fd1e
unknown <komronis01@gmail.com>
2023-11-15 19:21:59 +0600

    v1.2
    registration form is completed

--- autho.py
+++ autho.py
@@ -1,12 +1,7 @@
-import pymongo
 import tkinter as tk
 from tkinter import messagebox
-from time import sleep
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["admin"]
-mycol = mydb["users"]
-users_cred = {x['email']: x['password_'] for x in mycol.find()}
+import subprocess
+import pymongo
 
 def login():
     def validate_login():
@@ -21,12 +16,28 @@ def login():
             result_label.config(text="Invalid login or password", fg="red")
             tkWindow.login_status = False  # Set login status to False
 
+    def create_account():
+        # Open the registration form using subprocess
+        subprocess.Popen(["python", "reg.py"]).wait()
+        refresh_users_cred()
+
+    def refresh_users_cred():
+        # Refresh the users_cred dictionary from the database
+        users_cred.clear()
+        users_cred.update({x['email']: x['password_'] for x in mycol.find()})
+
+    # MongoDB connection
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["admin"]
+    mycol = mydb["users"]
+    users_cred = {x['email']: x['password_'] for x in mycol.find()}
+
     tkWindow = tk.Tk()
     tkWindow.geometry('400x150')
     tkWindow.title('Tkinter Login Form')
     tkWindow.login_status = None  # Initialize login status
 
-    username_label = tk.Label(tkWindow, text="User Name")
+    username_label = tk.Label(tkWindow, text="Email")
     username_label.grid(row=0, column=0)
     username_var = tk.StringVar()
     username_entry = tk.Entry(tkWindow, textvariable=username_var)
@@ -41,15 +52,18 @@ def login():
     login_button = tk.Button(tkWindow, text="Login", command=validate_login)
     login_button.grid(row=2, column=0, columnspan=2)
 
+    create_account_button = tk.Button(tkWindow, text="Create New Account", command=create_account)
+    create_account_button.grid(row=3, column=0, columnspan=2)
+
     result_label = tk.Label(tkWindow, text="", font=("Arial", 12))
-    result_label.grid(row=3, column=0, columnspan=2)
+    result_label.grid(row=4, column=0, columnspan=2)
 
     tkWindow.mainloop()
 
     return tkWindow.login_status  # Return the login status after the Tkinter main loop has ended
 
 if __name__ == '__main__':
-    is_authorized = autho.login()
+    is_authorized = login()
     if is_authorized:
         print("User is authorized.")
     else:
---
+++ reg.py
@@ -0,0 +1,85 @@
+import tkinter as tk
+from tkinter import messagebox
+import pymongo
+
+def register_user():
+    first_name = first_name_var.get()
+    last_name = last_name_var.get()
+    email = email_var.get()
+    gender = gender_var.get()
+    password = password_var.get()
+
+    # Validate that all fields are filled
+    if not all([first_name, last_name, email, gender, password]):
+        messagebox.showerror("Error", "All fields are required.")
+        return
+
+    # MongoDB connection
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["admin"]
+    mycol = mydb["users"]
+
+    # Check if the email already exists
+    if mycol.find_one({"email": email}):
+        messagebox.showerror("Error", "Email already exists. Please use a different email.")
+        return
+
+    # Insert new user into the database
+    new_user = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "gender": gender,
+        "password_": password
+    }
+
+    mycol.insert_one(new_user)
+    messagebox.showinfo("Success", "User registered successfully!")
+
+    # Clear the form
+    clear_form()
+    root.destroy()
+
+def clear_form():
+    first_name_var.set("")
+    last_name_var.set("")
+    email_var.set("")
+    gender_var.set("")
+    password_var.set("")
+
+# Create the main window
+root = tk.Tk()
+root.title("User Registration Form")
+root.geometry("400x250")
+
+# Variables for storing user input
+first_name_var = tk.StringVar()
+last_name_var = tk.StringVar()
+email_var = tk.StringVar()
+gender_var = tk.StringVar()
+password_var = tk.StringVar()
+
+# Labels and Entry widgets for the registration form
+tk.Label(root, text="First Name:").grid(row=0, column=0)
+tk.Entry(root, textvariable=first_name_var).grid(row=0, column=1)
+
+tk.Label(root, text="Last Name:").grid(row=1, column=0)
+tk.Entry(root, textvariable=last_name_var).grid(row=1, column=1)
+
+tk.Label(root, text="Email:").grid(row=2, column=0)
+tk.Entry(root, textvariable=email_var).grid(row=2, column=1)
+
+tk.Label(root, text="Gender:").grid(row=3, column=0)
+tk.Entry(root, textvariable=gender_var).grid(row=3, column=1)
+
+tk.Label(root, text="Password:").grid(row=4, column=0)
+tk.Entry(root, textvariable=password_var, show='*').grid(row=4, column=1)
+
+# Register button
+tk.Button(root, text="Register", command=register_user).grid(row=5, column=0, columnspan=2)
+
+# Clear button
+tk.Button(root, text="Clear", command=clear_form).grid(row=6, column=0, columnspan=2)
+
+# Start the Tkinter main loop
+root.mainloop()
